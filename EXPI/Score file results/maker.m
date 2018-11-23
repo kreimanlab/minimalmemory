c28c80ef802518c8496686647c240ace8135fbc9
misrapranav <misra.pranav@gmail.com>
2018-11-23 00:22:14 -0500

    3M corrections clean

--- EXPI/Score file results/maker.m
+++
@@ -1,8 +0,0 @@
-load('../subjectNameMappingMIT2.mat')
-for po1ooo = 1: size(kk,2)
-   clearvars -except kk ll po1ooo
-   load( fullfile('./',[kk{po1ooo} '.mat']));
-   fname = kk{po1ooo};
-   clear filename
-   save([kk{po1ooo} '.mat']);
-end
